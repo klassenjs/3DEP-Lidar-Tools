7728a6ed3b4aa3cc18dcee0383328c19580e6c6a
Jim Klassen <klassen.js@gmail.com>
2024-11-22 11:10:54 -0600

    Fix 3.Check_Download_Complete for OPR

--- 3.Check_Download_Complete.sh
+++ 3.Check_Download_Complete.sh
@@ -4,8 +4,12 @@
 for i in `find -name 0_file_download_links.txt | sort` ; do
 	shortname="${i%%/0_file_download_links.txt}"
 	shortname="${shortname##./vdelivery/Datasets/Staged/Elevation/}"
+	foldername=LAZ
+	if [[ $shortname = OPR* ]] ; then
+		foldername=TIFF
+	fi
 	echo \
 		$(cat $i | wc -l) \
-		$(find ${i/0_file_download_links.txt/LAZ} -maxdepth 1 -type f 2>/dev/null | wc -l) \
+		$(find ${i/0_file_download_links.txt/$foldername} -maxdepth 1 -type f 2>/dev/null | wc -l) \
 		${shortname}
 done
