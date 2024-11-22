ac72f1e1ad6a851611020567c8244c6ce3f8711a
Jim Klassen <klassen.js@gmail.com>
2024-11-22 11:20:10 -0600

    Colorize output of 3.Check_Download_Complete.sh

--- 3.Check_Download_Complete.sh
+++ 3.Check_Download_Complete.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+RED='\033[1;31m'
+NOR='\033[0m'
 
 # Check download complete
 for i in `find -name 0_file_download_links.txt | sort` ; do
@@ -8,8 +10,16 @@ for i in `find -name 0_file_download_links.txt | sort` ; do
 	if [[ $shortname = OPR* ]] ; then
 		foldername=TIFF
 	fi
-	echo \
-		$(cat $i | wc -l) \
-		$(find ${i/0_file_download_links.txt/$foldername} -maxdepth 1 -type f 2>/dev/null | wc -l) \
-		${shortname}
+	expected=$(cat $i | wc -l)
+	found=$(find ${i/0_file_download_links.txt/$foldername} -maxdepth 1 -type f 2>/dev/null | wc -l)
+	col="${NOR}"
+	if [[ $expected != $found ]] ; then
+		col="${RED}"
+	fi
+	echo -e \
+		${col} \
+		${expected} \
+		${found} \
+		${shortname} \
+		${NOR}
 done
