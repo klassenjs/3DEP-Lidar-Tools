637bd0ef49ef07bdbad43a1327ebdf78776e9cdb
Jim Klassen <klassen.js@gmail.com>
2024-12-02 11:46:28 -0600

    Remove extra boundary geojson column from gpkg

--- 7.Build_Tile_Index_With_Stats.sh
+++ 7.Build_Tile_Index_With_Stats.sh
@@ -13,6 +13,7 @@ python3 ../pdal_info_to_csv.py vdelivery > a.csv 2> a.err
 
 ## Convert the CSV table into a GeoPackage
 ogr2ogr -f GPKG MN-3DEP-Index-With-Tile-Stats.gpkg -oo GEOM_POSSIBLE_NAMES=boundary -a_srs EPSG:4326 -nln tindex a.csv a
+ogrinfo MN-3DEP-Index-With-Tile-Stats.gpkg -sql 'alter table tindex drop column boundary; vacuum;'
 
 ## See the output:
 # ogrinfo MN-3DEP-Index-With-Tile-Stats.gpkg -sql "alter table tindex drop column boundary"
