6621fcaacc0f6ad26b5f36a3cdcd917d1d8fe8a9
Jim Klassen <klassen.js@gmail.com>
2024-12-12 15:47:05 -0600

    5.Check_Valid_LAZ_Full.sh: Also fail empty JSON files.
    jq doesn't fail on an empty file, so check with grep.

--- 5.Check_Valid_LAZ_Full.sh
+++ 5.Check_Valid_LAZ_Full.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+RED='\033[1;31m'
+NOR='\033[0m'
 
 # Check files valid (full) and generate metadata JSONs for 7.Build_Tile_Index_With_Stats.sh
-for i in `find vdelivery/Datasets/Staged/Elevation/LPC/Projects/$1 -name 0_file_download_links.txt` ; do
+for i in `find vdelivery/Datasets/Staged/Elevation/LPC/Projects/"$1" -name 0_file_download_links.txt` ; do
    here="$(pwd)"
-   dir=${i/0_file_download_links.txt/LAZ}
+   dir="${i/0_file_download_links.txt/LAZ}"
    outdir="$(pwd)/lasinfo"
    echo "Checking $dir"
    (
@@ -19,7 +21,7 @@ for i in `find vdelivery/Datasets/Staged/Elevation/LPC/Projects/$1 -name 0_file_
             # Need this because some LAZ files have malformed CRS and causes PDAL to not emit the EPSG:4326 BBOX:
             # Note: this does slightly change the metadata output structure.
             #
-            sem -j+0 pdal pipeline "$here"/fix_projection.json --readers.las.filename="$j" --metadata /dev/stdout ">" "$outjson" "2>&1" "||" mv -v $j bad
+            sem -j+0 pdal pipeline "$here"/fix_projection.json --readers.las.filename="$j" --metadata /dev/stdout ">" "$outjson" "2>&1" "||" mv -v "$j" bad
         fi
      done
      rmdir --ignore-fail-on-non-empty bad
@@ -28,4 +30,14 @@ done
 sem --wait
 
 # Remove invalid JSONs
-for i in $(find lasinfo/vdelivery/Datasets/Staged/Elevation/LPC/Projects/$1 -name '*.json') ; do jq . $i > /dev/null || rm -v $i ; done
+echo "Searching and removing corrupt JSON files."
+echo "If this list is not empty, run this script again:"
+echo -en "${RED}"
+for i in $(find lasinfo/vdelivery/Datasets/Staged/Elevation/LPC/Projects/"$1" -name '*.json') ; do
+  # File is not "blank", and is readable as JSON (by jq), otherwise delete it.
+  grep -qF '"stages":' "$i" && \
+    jq .stages "$i" > /dev/null || rm -v "$i"
+done
+
+echo -en "${NOR}"
+echo "Done."
